6be804e11acc40f6d1498517c74ecadcf09c9b69
Jongjet Wongsarapee <jongjetw@192-168-1-126.tpgi.com.au>
2020-06-12 03:36:07 +1000

    register fn

--- Register.js
+++ Register.js
@@ -1,37 +1,57 @@
 import * as React from 'react';
+import {useState} from 'react';
 import { Text, View, Image, StyleSheet, TextInput } from 'react-native';
 import { Button } from 'react-native-paper';
+import axios from 'axios';
 
 export function Register({ navigation, route }) {
+
+    const [user, setUser] = useState('');
+    const [pass, setPass] = useState('');
+    const [pNo, setPNo] = useState('');
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 
             <Text style={{ paddingTop: 20 }}>Usename</Text>
             <TextInput
                 style={{ height: 40, width: 100, borderColor: 'gray', borderWidth: 1 }}
-            //onChangeText={text => onChangeText(text)}
-            //value={value}
+                onChangeText={user => setUser(user)}
+                defaultValue={user}
             />
 
             <Text>Password</Text>
             <TextInput
                 style={{ height: 40, width: 100, borderColor: 'gray', borderWidth: 1 }}
-            //onChangeText={text => onChangeText(text)}
-            //value={value}
+                onChangeText={pass => setPass(pass)}
+                defaultValue={pass}
             />
 
             <Text>Telephone No</Text>
             <TextInput
                 style={{ height: 40, width: 100, borderColor: 'gray', borderWidth: 1 }}
-            //onChangeText={text => onChangeText(text)}
-            //value={value}
+                onChangeText={pNo => setPNo(pNo)}
+                defaultValue={pNo}
             />
 
             <Button
                 icon="account-plus"
                 style={{ marginTop: 30 }}
                 mode="contained"
-                onPress={() => navigation.navigate("Register")}>
+                onPress={() => {
+                    
+                    axios
+                    .post('http://192.168.1.126:3000/user/',{
+                        username: user,
+                        password: pass
+                    })
+                    .then(function(response){
+                        console.log(response.data);
+                    })
+                    .catch(function(error){
+                        console.log(error);
+                    })
+                }}>
                 Register
             </Button>
         </View>
